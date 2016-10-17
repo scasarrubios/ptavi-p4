44e6f4e93fc214ddfa6c0c80453333c672b8d101
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-10-18 00:28:52 +0200

    register y dicc de clientes añadidos. recorte de usuario muy aparatoso

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
 
 import socketserver
 import sys
@@ -12,11 +9,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    clients = {}
     def handle(self):
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b" ")
         for line in self.rfile:
+            if line.decode('utf-8')[0:8] == 'REGISTER':
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                self.clients[line.decode('utf-8')[line.decode('utf-8').find(':')+1:line.decode('utf-8').rfind(' ')]] = self.client_address
             print("El cliente", self.client_address, "nos manda", line.decode('utf-8'))
+        print(self.clients)
 
 if __name__ == "__main__":
     serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print("Finalizado servidor")
+        print("\nFinalizado servidor")
