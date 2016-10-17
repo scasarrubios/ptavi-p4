2bb83af7e37c378b1049ae1accbd525754992e80
Sergio Casarrubios De Arcos <scasarru@gamma07.aulas.gsyc.urjc.es>
2016-10-17 10:44:44 +0200

    Server coge el puert de la shell e imprime IP y puerto de cliente

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -15,10 +16,10 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            print("El cliente", self.client_address, "nos manda", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
