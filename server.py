884b8f2e160f0a7b9822c29b8bdfcf1ed5d2069d
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-10-18 23:51:06 +0200

    Cambiada la lectura de mensajes

--- server.py
+++ server.py
@@ -12,11 +12,12 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     clients = {}
     def handle(self):
         self.wfile.write(b" ")
-        for line in self.rfile:
-            if line.decode('utf-8')[0:8] == 'REGISTER':
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                self.clients[line.decode('utf-8')[line.decode('utf-8').find(':')+1:line.decode('utf-8').rfind(' ')]] = self.client_address
-            print("El cliente", self.client_address, "nos manda", line.decode('utf-8'))
+        line = self.rfile.read().decode('utf-8').split()
+        if line[0] == 'REGISTER':
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            self.clients[line[1][4:]] = self.client_address[0]
+        else:
+            print("El cliente", self.client_address, "nos manda:", ' '.join(line))
         print(self.clients)
 
 if __name__ == "__main__":
