263d1223f81692eb6e0c7bffec1d34549e7d7899
Sergio Casarrubios De Arcos <scasarru@gamma30.aulas.gsyc.urjc.es>
2016-10-24 10:35:39 +0200

    pep8 y docstrings realizado

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente UDP que abre un socket a un servidor
+Programa cliente SIP que abre un socket a un servidor
 """
 
 import socket
@@ -21,11 +21,11 @@ except:
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", line)
     if sys.argv[3] == 'register':
         line = "REGISTER sip:" + name + " SIP/2.0\r\nExpires: " + expires
     my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
+    print("Enviando:", line)
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Programa servidor SIP
+"""
 
 import socketserver
 import sys
@@ -13,7 +16,12 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     clients = {}
     no_file = False
+
     def caducity_check(self, line):
+        """
+        Compara la fecha de caducidad de los clientes del diccionario,
+        y si han caducado los añade a la lista caduced para borrarlos
+        """
         caduced = []
         for client in self.clients:
             now = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
@@ -21,21 +29,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 caduced.append(client)
         for client in caduced:
             del self.clients[client]
-            
+
     def register2json(self):
-        #dicc = {}
+        """
+        Imprime el diccionario de clientes en un fichero json
+        """
         if self.no_file:
             fichero = open('registered.json', 'w')
         else:
             fichero = open('registered.json', 'r+')
-       # for client in self.clients:
-        #   data = {}
-         #  data['address'] = self.clients[client][0]
-          # data['expires'] = self.clients[client][1]
-           #dicc[client] = data
-        json.dump(self.clients, fichero)
-    
+        json.dump(self.clients, fichero, indent='\t')
+
     def json2register(self):
+        """
+        Comprueba si hay un fichero de json y si lo hay importa los clientes,
+        si no cambia el valor de no_file a True
+        """
         try:
             with open('registered.json') as data_file:
                 self.clients = json.load(data_file)
@@ -43,7 +52,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         except:
             self.no_file = True
             print(self.no_file)
-                
+
     def handle(self):
         self.json2register()
         self.wfile.write(b" ")
@@ -58,11 +67,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             elif line[3] == 'Expires:' and line[4] != '0':
                 caduc_time = time.gmtime(time.time()+int(line[4]))
                 data.append(time.strftime('%Y-%m-%d %H:%M:%S', caduc_time))
-            self.clients[line[1][4:]] = data
+                self.clients[line[1][4:]] = data
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             self.register2json()
-        else:
-            print("El cliente", self.client_address, "nos manda:", ' '.join(line))
         print(self.clients)
 
 if __name__ == "__main__":
